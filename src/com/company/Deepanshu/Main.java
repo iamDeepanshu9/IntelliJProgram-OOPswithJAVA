958c14ae1191e30dc2b98b762fa8f41a9ef0e872
iamdeepanshu9 <iamdeepanshu9@gmail.com>
2021-12-12 22:30:46 +0530

    nothing extra

---
+++ src/com/company/Deepanshu/Main.java
@@ -0,0 +1,10 @@
+package com.company.Deepanshu;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int x=3&5;
+    int y = 3|5;
+        System.out.println(x + " " + y);
+    }
+}
